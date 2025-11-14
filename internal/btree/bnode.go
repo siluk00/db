23293ea8fc9318e1b82e9c7ec40233249484ec87
siluk00 <sil-uk@hotmail.com>
2025-11-14 03:25:56 -0300

    node functions

---
+++ internal/btree/bnode.go
@@ -0,0 +1,148 @@
+package btree
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// header consists of bNode + number of Keys
+const HEADER = 4
+
+type BNode []byte
+
+// bType types
+const (
+	BNODE_NODE = 1
+	BNODE_LEAF = 2
+)
+
+func (node BNode) bType() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nKeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype, nKeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nKeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	if !(idx < node.nKeys()) {
+		panic("nKeys should be greater then index")
+	}
+
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+// HEY
+func (node BNode) setPtr(idx uint16, val uint64) {
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node[pos:], val)
+}
+
+func offsetPos(node BNode, idx uint16) uint16 {
+	if !(1 <= idx && idx <= node.nKeys()) {
+		panic("")
+	}
+
+	return HEADER + 8*node.nKeys() + 2*(idx-1)
+}
+
+// if the index is zero, the offset position of the kv pair is just 0;
+// offset implements the position of the kv pair inside the BNode struct
+// so the search is O(1)
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+
+	return binary.LittleEndian.Uint16(node[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx, offset uint16) {
+
+}
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	if !(idx <= node.nKeys()) {
+		panic("")
+	}
+
+	return HEADER + 8*node.nKeys() + 2*node.nKeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	if !(idx < node.nKeys()) {
+		panic("")
+	}
+
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos:])
+	return node[pos+4:][:klen]
+}
+
+// TODO
+func (node BNode) getVal(idx uint16) []byte {
+	return []byte{0}
+}
+
+func (node BNode) nBytes() uint16 {
+	return node.kvPos(node.nKeys())
+}
+
+// TODO: Binary Search
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nKeys := node.nKeys()
+	found := uint16(0)
+
+	for i := uint16(1); i < nKeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp <= 0 {
+			found = i
+		}
+
+		if cmp >= 0 {
+			break
+		}
+	}
+
+	return found
+}
+
+func leafInsert(newBNode, oldBNode BNode, idx uint16, key, val []byte) {
+	newBNode.setHeader(BNODE_LEAF, oldBNode.nKeys()+1)
+	nodeAppendRange(newBNode, oldBNode, 0, 0, idx)
+	nodeAppendKV(newBNode, idx, 0, key, val)
+	nodeAppendRange(newBNode, oldBNode, idx+1, idx, oldBNode.nKeys()-idx)
+
+}
+
+func nodeAppendKV(newBNode BNode, idx uint16, ptr uint64, key, val []byte) {
+	newBNode.setPtr(idx, uint64(ptr))
+
+	pos := newBNode.kvPos(idx)
+	binary.LittleEndian.PutUint16(newBNode[pos:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(newBNode[pos+2:], uint16(len(val)))
+	copy(newBNode[pos+4:], key)
+	copy(newBNode[pos+4+uint16(len(key)):], val)
+
+	newBNode.setOffset(idx+1, uint16(newBNode.getOffset(idx)+4+uint16((len(key)+len(val)))))
+}
+
+func nodeAppendRange(newBNode, oldBNode BNode, dstNew, srcOld, n uint16) {
+
+}
+
+func nodeAppendKidN(tree *BTree, newBNode, oldBNode BNode, idx uint16, kids ...BNode) {
+	inc := uint16(len(kids))
+	newBNode.setHeader(BNODE_NODE, oldBNode.nKeys()+inc-1)
+	nodeAppendRange(newBNode, oldBNode, 0, 0, idx)
+	for i, node := range kids {
+		nodeAppendKV(newBNode, idx+uint16(i), tree.newBTree(node), node.getKey(0), nil)
+	}
+	nodeAppendRange(newBNode, oldBNode, idx+inc, idx+1, oldBNode.nKeys()-(idx+1))
+}
