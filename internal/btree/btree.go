8c98d8a7d267cb45de49e81b44b87d8df33ed0ff
siluk00 <sil-uk@hotmail.com>
2025-11-17 17:38:47 -0300

    pre tests

--- internal/btree/btree.go
+++ internal/btree/btree.go
@@ -34,3 +34,78 @@ func (tree *BTree) Insert(key, val []byte) {
 func (tree *BTree) Delete(key []byte) bool {
 	return false
 }
+
+// remove a key from a leaf node
+func leafDelete(new BNode, old BNode, idx uint16)
+
+// merge 2 nodes into 1
+func nodeMerge(new BNode, left BNode, right BNode)
+
+// replace 2 adjacent links with 1
+func nodeReplace2Kid(
+	new BNode, old BNode, idx uint16, ptr uint64, key []byte,
+)
+
+func shouldMerge(tree *BTree, node BNode, idx uint16, updated BNode) (int, BNode) {
+	if updated.nBytes() > BTREE_PAGE_SIZE/4 {
+		return 0, BNode{}
+	}
+
+	if idx > 0 {
+		sibling := BNode(tree.get(node.getPtr(idx - 1)))
+		merged := sibling.nBytes() + updated.nBytes() - HEADER
+
+		if merged <= BTREE_PAGE_SIZE {
+			return -1, sibling
+		}
+	}
+
+	if idx+1 < node.nKeys() {
+		sibling := BNode(tree.get(node.getPtr(idx + 1)))
+		merged := sibling.nBytes() + updated.nBytes() - HEADER
+		if merged <= BTREE_PAGE_SIZE {
+			return +1, sibling
+		}
+	}
+
+	return 0, BNode{}
+}
+
+// delete a key from the tree
+func treeDelete(tree *BTree, node BNode, key []byte) BNode {
+	return BNode{}
+}
+
+func nodeDelete(tree *BTree, node BNode, idx uint16, key []byte) BNode {
+	kptr := node.getPtr(idx)
+	updated := treeDelete(tree, tree.get(kptr), key)
+
+	if len(updated) == 0 {
+		return BNode{}
+	}
+	tree.del(kptr)
+
+	newBnode := BNode(make([]byte, BTREE_PAGE_SIZE))
+	mergeDir, sibling := shouldMerge(tree, node, idx, updated)
+	switch {
+	case mergeDir < 0:
+		merged := BNode(make([]byte, BTREE_PAGE_SIZE))
+		nodeMerge(merged, sibling, updated)
+		tree.del(node.getPtr(idx - 1))
+		nodeReplace2Kid(newBnode, node, idx-1, tree.newBTree(merged), merged.getKey(0))
+	case mergeDir > 0:
+		merged := BNode(make([]byte, BTREE_PAGE_SIZE))
+		nodeMerge(merged, updated, sibling)
+		tree.del(node.getPtr(idx + 1))
+		nodeReplace2Kid(newBnode, node, idx, tree.newBTree(merged), merged.getKey(0))
+	case mergeDir == 0 && updated.nKeys() == 0:
+		if !(node.nKeys() == 1 && idx == 0) {
+			panic("")
+		}
+		newBnode.setHeader(BNODE_NODE, 0)
+	case mergeDir == 0 && updated.nKeys() > 0:
+		nodeReplaceKidN(tree, newBnode, node, idx, updated)
+	}
+
+	return newBnode
+}
---
+++ internal/btree/btreeTest.go
@@ -0,0 +1,43 @@
+package btree
+
+import "unsafe"
+
+type C struct {
+	tree  BTree
+	ref   map[string]string
+	pages map[uint64]BNode
+}
+
+func newC() *C {
+	pages := map[uint64]BNode{}
+	return &C{
+		tree: BTree{
+			get: func(ptr uint64) []byte {
+				node, ok := pages[ptr]
+				if !ok {
+					panic("")
+				}
+				return node
+			},
+			newBTree: func(node []byte) uint64 {
+				if !(BNode(node).nBytes() <= BTREE_PAGE_SIZE) {
+					panic("")
+				}
+				ptr := uint64(uintptr(unsafe.Pointer(&node[0])))
+				if !(pages[ptr] == nil) {
+					panic("")
+				}
+				pages[ptr] = node
+				return ptr
+			},
+			del: func(ptr uint64) {
+				if !(pages[ptr] != nil) {
+					panic("")
+				}
+				delete(pages, ptr)
+			},
+		},
+		ref:   map[string]string{},
+		pages: pages,
+	}
+}
